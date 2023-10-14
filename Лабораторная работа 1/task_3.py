94b6099cc080d5f54b470088aaae6515f66b602e
Tem0mie <147901565+Tem0mie@users.noreply.github.com>
2023-10-14 13:09:35 +0700

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,12 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+
+count_players = len(list_players)
+print(count_players)
+
+first_team = list_players[:3]
+second_team = list_players[3:]
+
+print(first_team)
+print(second_team)
